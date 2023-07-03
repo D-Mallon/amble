39209ee5a2ae4747e7285c1b872479d0d34302b9
cormacelynch <cormac.lynch1@ucdconnect.ie>
2023-07-03 09:57:00 +0100

    readme login file added

--- users/views.py
+++ users/views.py
@@ -10,15 +10,11 @@ from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 def user_view(request):
     if request.method == 'GET':
         met = 'GET'
-    #     user = User.objects.all()
-    #     serializer = UserSerializer(user, many=True)
-    #     return Response(serializer.data)
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        # met = 'POST'
-        # response_message = f'Method = {met}'
-        # return HttpResponse(response_message)
-    
         data = User.objects.all()
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -27,12 +23,3 @@ def user_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     return HttpResponseBadRequest('Unsupported request method.')
-
-        # # POST the submitted data
-        # first_name = request.POST.get('first_name')
-        # last_name = request.POST.get('last_name')
-        # email = request.POST.get('email')
-        # address = request.POST.get('address')
-        # password = request.POST.get('password')
-        # response_message = f'Method = {met} , Name: {first_name} {last_name} and Email: {email}'
-        # return HttpResponse(response_message)
