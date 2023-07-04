dcc3df2d00316921618c380558281b955c59d855
cormacelynch <cormac.lynch1@ucdconnect.ie>
2023-07-04 17:57:23 +0100

    commit

--- users/urls.py
+++ users/urls.py
@@ -4,15 +4,14 @@ from users import views
 
 urlpatterns = [
     
-    path('users', views.handle_routeinput_data),
+    path('users', views.user_route),
     path('users', views.user_view),
     
     # re_path(r'^users/$', views.user_view),
     # re_path(r'^api/users/(?P<pk>[0-9]+)$', views.user_view),
-    
     # path('users', views.handle_routeinpput_data),
-#     re_path(r'^users/$', views.handle_routeinpput_data),
-#     re_path(r'^api/users/(?P<pk>[0-9]+)$', views.handle_routeinpput_data),
+    # re_path(r'^users/$', views.handle_routeinpput_data),
+    # re_path(r'^api/users/(?P<pk>[0-9]+)$', views.handle_routeinpput_data),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .models import User
-from .serializers import UserSerializer
+from .models import User, UserRoute
+from .serializers import UserSerializer, UserRouteSerializer
 from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 
 #Function to view user input data
@@ -23,16 +23,35 @@ def user_view(request):
     
     return HttpResponseBadRequest('Unsupported request method.')
 
-def handle_routeinput_data(request):
-    if request.method == 'POST':
-        latitude = request.POST.get('latitude')
-        longitude = request.POST.get('longitude')
-        distance = request.POST.get('distance')
+#Function to view user route data
+@api_view(['GET', 'POST'])
+def user_route(request):
+    if request.method == 'GET':
+            userroute = UserRoute.objects.all()
+            serializer = UserRouteSerializer(user, many=True)
+            return Response(serializer.data)
 
-        print(f'Latitude: {latitude} Longitude: {longitude} Distance: {distance} ') 
-        print(f'Latitude: {type(latitude)} ') 
+    elif request.method == 'POST':
+        data = User.objects.all()
+        serializer = UserRouteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    return HttpResponseBadRequest('Unsupported request method.')
 
-        response_data = {'latitude': latitude, 'longitude':longitude, 'distance':distance}
-        # response_data = {'message': 'Form data received successfully'}
-        print(response_data)
-        return JsonResponse(response_data)
+
+
+    # if request.method == 'POST':
+    #     latitude = request.POST.get('latitude')
+    #     longitude = request.POST.get('longitude')
+    #     distance = request.POST.get('distance')
+
+    #     print(f'Latitude: {latitude} Longitude: {longitude} Distance: {distance} ') 
+    #     print(f'Latitude: {type(latitude)} ') 
+
+    #     response_data = {'latitude': latitude, 'longitude':longitude, 'distance':distance}
+    #     # response_data = {'message': 'Form data received successfully'}
+    #     print(response_data)
+    #     return JsonResponse(response_data)
