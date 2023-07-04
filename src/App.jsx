d53b90305cccb63f67ba0af29e61898ac8393e47
Finbar Allan <116646659+finbarallan@users.noreply.github.com>
2023-07-04 14:24:08 +0100

    dev-cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Interface from "./components/Interface";
 import "./App.css";
 
-import { createTheme } from '@mui/material/styles';
-import StartPlace from "./components/start_place";
-import StartTime from './components/start_time';
-import Preference from './components/preference';
-import Distance from './components/walk_distance';
-
+import Interface from "./components/Interface";
 import Login from "./components/login";
 
 function App() {
