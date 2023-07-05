5bf2bf1bdbc2d960542645a880108d41db3e1234
cormacelynch <cormac.lynch1@ucdconnect.ie>
2023-07-05 10:49:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,65 +19,14 @@ function App() {
     -73.98786664009094]);
 
   return (
-    <div>
-      <img
-        src="/static/images/menu.png"
-        className="menu-button"
-        alt="Menu button"
-      />
-
-      <div className="PlanWin">
-        <div className="PlanSet">
-          <div className="green-bar">
-            <span className="text_bar">Walking Setting</span>
-          </div>
-
-          <div className="start-place">
-            <span className="text_bar_3">Start Place:</span>
-            <StartPlace inputValues={inputValues} setInputValues={setInputValues}/>
-          </div>
-
-          <div className="start-time">
-            <span className="text_bar_3">Start Time:</span>
-            <StartTime />
-          </div>
-
-          <div className="walk-duration">
-            <span className="text_bar_3">Distance:</span>
-            <span className="spacer1">&nbsp;</span>
-            <Distance />
-          </div>
-
-
-          <div className="preference">
-            <span className="text_bar_3">Preference:</span>
-            < Preference />
-          </div>
-
-          <div className="gain-route">
-            <button className="gainroute-button">Gain a Route</button>
-          </div>
-        </div>
-
-        <div className="PlanMap">
-          <div className="additional-blocks">
-            <div className="additional-block-text">
-              <span className="text_bar_2">Choose Position</span>
-            </div>
-            <div className="additional-block-close">
-              <img
-                src="/static/images/close.png"
-                className="close-button"
-                alt="Mclose button"
-              />
-            </div>
-          </div>
-          {/* <Map inputValues={inputValues}/> */}
-          {/* <Login /> */}
-          <RouteInputs />
-        </div>
-      </div>
-    </div>
+<div>
+    <Router>
+    <Routes>
+      <Route exact path="/" element={<Interface />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router >
+</div >
   );
 }
 
