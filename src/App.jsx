9f7ce8146a58a270600dcf5c2c1494abcf6af055
cormacelynch <cormac.lynch1@ucdconnect.ie>
2023-07-25 13:02:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ function App() {
   const [inputValues, setInputValues] = useState({
     "latitude": 40.74218481889335,
     "longitude": -73.98786664009094,
-    "endLatitude": 40.72540497175606,
-    "endLongitude": -74.01052594184875,
+    // "endLatitude": 40.72540497175606,
+    // "endLongitude": -74.01052594184875,
     "endLatitude": 40.72540497175606,
     "endLongitude": -74.01052594184875,
     "hour": 0,
