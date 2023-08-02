215b756d04e5fff69587e4a6da6fba03c326a5f6
Finbar Allan <116646659+finbarallan@users.noreply.github.com>
2023-08-02 13:33:06 +0100

    functional-favicons

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginFaviconsInject('./vite.svg'),
+  ],
   publicDir: './public',
   optimizeDeps: {
     exclude: []
