5db7e5dec6d3e4ec2115b5901836c7bb2bb2c8a5
cormacelynch <cormac.lynch1@ucdconnect.ie>
2023-08-03 11:48:00 +0100

    crime

--- scripts/update_busy.py
+++ scripts/update_busy.py
@@ -3,6 +3,7 @@ from os import path
 import os
 import time
 import datetime
+import statistics
 
 ####### Start time - to get run time #########
 start_time = time.time()
@@ -11,17 +12,36 @@ start_time = time.time()
 def openJson(dir,file):
     if path.isfile(os.path.join(dir, file)) is False:
         raise Exception(f"{file} File not found")
-    
     with open(os.path.join(dir, file)) as f:
         return(json.load(f))
     
-#Function fo get b-scores
+#Function to get b-scores
 def getBusy(taxizone):
     all_hours = {}
     for d in busyObj:
         if d["Taxi Zone ID"] == taxizone:
             all_hours[d["Hour"]] = round(d["Busyness Predicted"],4)
-    return(all_hours)    
+    return(all_hours)   
+
+#Function to get c-scores
+def getCrime():
+    crime_score = {}
+    for d in busyObj_crime['data']:
+        crime_score[d["taxi-zone"]] = d["c-score"]
+
+    list_crime_scores = list(crime_score.values())
+
+    mean_crime = statistics.mean(list_crime_scores)
+    median_crime = statistics.median(list_crime_scores)
+    range_crime = max(list_crime_scores)-min(list_crime_scores)
+    std_crime = statistics.stdev(list_crime_scores)
+    # Print the results
+    print("\nMean of Crime Scores: ", mean_crime)
+    print("Median of Crime Scores: ", median_crime)
+    print("Range of Crime Scores: ", range_crime)
+    print("Standard Deviation of Crime Scores: ", std_crime)
+    # print(list_crime_scores)
+    return(crime_score)   
 
 #Function to Update Nodes Busyness Scores  (incorporates some of the earlier functions)
 def update_nodes(nodes):
@@ -59,20 +79,20 @@ def create_ts():
     month = nyc_time.month
     day = nyc_time.day
     hour= nyc_time.hour
-
-    #Create Date and Time variables for use in the Pickle File
     xdate = datetime.datetime(year, month, day, hour) #Produces datetime object
     dow = xdate.weekday() #Produces Day of the Week
-    # print(f'Day of the week = {dow}')
     timestamp = datetime.datetime.timestamp(xdate) #Produces Timestamp Object.
-
     return(timestamp)
 
 #Get path to json directory
 json_dir = r"src\json-files" 
 
-#json busyness file
-busy = 'busy_taxi_final.json'
+#json busyness files
+busy_taxi = 'busy_taxi_final.json'
+busy_bike = 'busy_bike_final.json'
+
+#All Node Files
+crime = 'all_nodes.json'
 
 #json location files
 park = 'park_locations.json'
@@ -83,22 +103,31 @@ museum = 'museum_art_locations.json'
 worship = 'worship_locations.json'
 walking_node = 'walking_node_locations.json'
 
-#Create path to busyness json file and open it
-busyObj = openJson(json_dir,busy)
-if path.isfile(os.path.join(json_dir, busy)) is False:
-    raise Exception(f"{busy} File not found")
+#Open taxi busyness json file
+busyObj= openJson(json_dir,busy_taxi)
 
-with open(os.path.join(json_dir, busy)) as f:
-    busyObj = json.load(f)
+#Open bike busyness json file
+busyObj_bike = openJson(json_dir,busy_bike)
 
-#Update the Nodes
-update_nodes(park)
-update_nodes(library)
-update_nodes(parknode)
-update_nodes(community)
-update_nodes(museum)
-update_nodes(worship)
-update_nodes(walking_node)
+#Open all nodes json file for crime score
+busyObj_crime = openJson(json_dir,crime)
+
+print(getCrime())
+print(len(getCrime()))
+
+#Weightings for Busyness
+taxi_weight = 0.70
+bike_weight = 0.20
+crime_weight = 0.10
+
+# #Update the Nodes
+# update_nodes(park)
+# update_nodes(library)
+# update_nodes(parknode)
+# update_nodes(community)
+# update_nodes(museum)
+# update_nodes(worship)
+# update_nodes(walking_node)
 
 # #Create paths to json files and open them
 # busyObj = openJson(json_dir,file_busy)
@@ -139,3 +168,13 @@ update_nodes(walking_node)
 end_time = time.time()
 run_time = round((end_time - start_time),1)
 print(f'Run time to populate busyness scores for all 24 hours = {run_time} seconds')
+
+# if path.isfile(os.path.join(json_dir, busy_taxi)) is False:
+#     raise Exception(f"{busy_taxi} File not found")
+# with open(os.path.join(json_dir, busy_taxi)) as f:
+#     busyObj_taxi = json.load(f)
+
+# if path.isfile(os.path.join(json_dir, busy_bike)) is False:
+#     raise Exception(f"{busy_bike} File not found")
+# with open(os.path.join(json_dir, busy_bike)) as f:
+#     busyObj_bike = json.load(f)
