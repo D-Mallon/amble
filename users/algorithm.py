e5727450588ad3ddc81e8edfab23fb6507bf0971
cormacelynch <cormac.lynch1@ucdconnect.ie>
2023-08-04 16:47:57 +0100

    crime

--- users/algorithm.py
+++ users/algorithm.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ####### Start time - to get run time #########
 # start_time = time.time()
 
-#Set up the base nodes (from Park Locations)
+#Get the base nodes (from Park Locations)
 file_path_par = BASE_DIR /'src'/'json-files'/'park_locations.json'
 with open(file_path_par) as json_file:
     basedata = json.load(json_file)
@@ -19,7 +19,7 @@ with open(file_path_par) as json_file:
 data = {}
 data.update(basedata)
 
-#Add other park nodes
+#Get and add other park nodes to base nodes
 file_path_oth_park = BASE_DIR /'src'/'json-files'/'park_node_locations.json'
 with open(file_path_oth_park) as file:
     other_park = json.load(file)
@@ -33,20 +33,17 @@ with open(file_path_pre) as json_file:
 t = True
 for x in prefdata["data_from_frontend"]["selectedOptions"]:
     other_nodes_dict.update({x:t})
-    # print(other_nodes_dict)
 
-#Add the nodes
+#Add the nodes from preferences
 for k,v in other_nodes_dict.items():
     if v == True:
         f = k+'.json'
         file_path_oth = BASE_DIR /'src'/'json-files'/f
         with open(file_path_oth) as file:
             nodes = json.load(file)
-        # print(nodes)
-        # print(type(nodes))
         data ={'data':data['data'] + nodes['data']}
         
-# #Create a json object and Write to a json file
+#Create a json object and Write to a json file
 merged_json = json.dumps(data, indent=4) 
 file_path_mer = BASE_DIR /'src'/'json-files'/'nodes_final.json'
 with open(file_path_mer, 'w') as merged_file: 
