14caf8320c7f711c09a426b575bc2c8c05cd793d
Finbar Allan <116646659+finbarallan@users.noreply.github.com>
2023-08-04 12:09:15 +0100

    clickable-markers

--- src/components/useMapInit.jsx
+++ src/components/useMapInit.jsx
@@ -6,6 +6,14 @@ import communities from '../json-files/community_locations.json';
 import museums from '../json-files/museum_art_locations.json';
 import worships from '../json-files/worship_locations.json';
 
+const toTitleCase = (str) => {
+  if (str === 'null' || str === undefined) return '';
+  str = str.replace(/;/g, ' '); // Replace all semicolons with spaces
+  return str.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
 const useMapInit = (mapContainer, lat, lng, zoom, inputValues) => {
   const map = useRef(null);
   const [markers, setMarkers] = useState([]);
@@ -75,6 +83,10 @@ const useMapInit = (mapContainer, lat, lng, zoom, inputValues) => {
       const newMarkers = data.map((item) => ({
         id: item.id,
         name: item.name,
+        address: item.address,
+        internet_access: item.internet_access,
+        wheelchair_accessible: item.wheelchair_accessible,
+        opening_hours: item.opening_hours,
         latitude: item.location.latitude,
         longitude: item.location.longitude,
       }));
@@ -102,6 +114,10 @@ const useMapInit = (mapContainer, lat, lng, zoom, inputValues) => {
                   properties: {
                     id: marker.id,
                     name: marker.name,
+                    address: marker.address,
+                    internet_access: marker.internet_access,
+                    wheelchair_accessible: marker.wheelchair_accessible,
+                    opening_hours: marker.opening_hours,
                   },
                 })),
               },
@@ -145,14 +161,22 @@ const useMapInit = (mapContainer, lat, lng, zoom, inputValues) => {
         const layers = ['park', 'library', 'community', 'museum', 'worship'];
         const features = map.current.queryRenderedFeatures(e.point, { layers });
 
-        if (features.length > 0) {
+        if (features.length > 0 && features[0] !== null && features[0] !== "null" && typeof features[0] !== 'undefined') {
           const coordinates = features[0].geometry.coordinates.slice();
-          const popupContent = features[0].properties.name;
-          const id = features[0].properties.id;
+          const { name, id, address, internet_access, wheelchair_accessible, opening_hours } = features[0].properties;
+          const popupContent = `<div class="popup-content">
+          <h3>${name}</h3>
+          <p>
+          ${address !== null && address !== 'null' && address !== undefined ? `Address: ${address}<br>` : ''}
+          ${internet_access !== null && internet_access !== 'null' && internet_access !== undefined ? `Internet Access: ${toTitleCase(internet_access)}<br>` : ''}
+          ${wheelchair_accessible !== null && wheelchair_accessible !== 'null' && wheelchair_accessible !== undefined ? `Wheelchair Accessible: ${toTitleCase(wheelchair_accessible)}<br>` : ''}
+          ${opening_hours !== null && opening_hours !== 'null' && opening_hours !== undefined ? `Opening Hours: ${opening_hours}<br>` : ''}
+          </p>
+        </div>`;
 
           const popup = new mapboxgl.Popup({ className: 'custom-popup' })
             .setLngLat(coordinates)
-            .setHTML(`<div class="popup-content">${popupContent}<br>${id}<br>${coordinates}</div>`)
+            .setHTML(popupContent)
             .addTo(map.current);
           setPopup(popup);
         }
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    vitePluginFaviconsInject('./vite.svg'),
   ],
   publicDir: './public',
   optimizeDeps: {
