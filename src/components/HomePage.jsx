cf42b2fcc11b43a85b1f3636e56ef7210c4eaa73
Finbar Allan <116646659+finbarallan@users.noreply.github.com>
2023-08-06 19:24:57 +0100

    vite-logo-fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,5 @@
 import React, { useRef, useEffect, useState,useContext } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
-const reactLogo = "react.svg";
-const viteLogo = "vite.svg";
 
 // import StartPlace from "./components/start_place";
 
